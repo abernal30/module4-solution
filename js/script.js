1e99d81a85e6fdd82b95d8f01a6f1aa89e31637a
Luis Arturo Bernal Ponce <larturo.bernal@tec.mx>
2021-09-05 01:11:26 -0500

    small change

--- js/script.js
+++ js/script.js
@@ -3,14 +3,12 @@
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-/
-// See Lecture 50, part 1
+
 for (var i = 0; i < names.length; i++) {
 
     var firstLetter = names[i].charAt(0).toLowerCase();
 
-  
-  // name in the loop.
+
   if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
   } else {
